e35fd2427220517d59cde136d8a08940eb7f4720
Montana <montanacraven@Gmail.com>
2021-06-15 14:39:31 +0100

    testing3

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -34,7 +34,7 @@ function MainContent(): JSX.Element {
         </p>
         <h4>Stats</h4>
         <JournalEntry
-        views={20000}
+        views={24000}
         likes={2000}
         postDate={{ month: "March", day: 2, year: 2003 }}
       />
